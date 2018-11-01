dcac7de58fd0820b47e0891b94d80b0cc5dcc588
Muhammad <m.saleem.y2k@gmail.com>
2018-11-01 13:17:48 +0300

    add readme docs

--- import/ui/Login.js
+++ import/ui/Login.js
@@ -29,7 +29,7 @@ render(){
 return (
   <div className="boxed-view">
   <div className="boxed-view__box">
-    <h1>Short Link</h1>
+    <h1>Short Link Login</h1>
     { this.state.error ? <p>{this.state.error}</p> : undefined }
     <form onSubmit={this.onSubmit.bind(this)} noValidate className="boxed-view__form" >
    <input type="email" ref="email" name="email" placeholder="Email"/>
